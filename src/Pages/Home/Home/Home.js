5c0dfba8e4e4b7ecdc83cf2f2ede776d65c727cc
shuvornoy <shuvornoy@gmaol.com>
2022-11-09 05:20:07 -0800

    contact add

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Info from '../../Shared/Info/Info';
 import Banner from '../Banner/Banner';
+import Contact from '../Contact/Contact';
 import MakeAppointment from '../MarkAppoment/MarkAppoment';
 import HomeServices from '../Services/HomeService';
 
@@ -14,6 +15,7 @@ const Home = () => {
            <button className="btn btn-outline px-10">  <a href="/services">SEE ALL</a> </button>
            <Info />
            <MakeAppointment />
+           <Contact />
            </div>
         </div>
     );
